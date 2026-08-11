626dba816612c616c1bad66d2037dd09c1bc8af8
Mamdouh-Helmy <mm9349263@gmail.com>
2026-08-11 06:50:38 +0300

    @new@

--- src/app/(site)/portfolio/[id]/layout.tsx
+++ src/app/(site)/portfolio/[id]/layout.tsx
@@ -4,7 +4,6 @@ import { fetchPortfolio } from "@/lib/fetchPortfolio";
 import { PortfolioProvider } from "../../../../../context/PortfolioContext";
 import Header from "../../../../components/Portfolio/public/Header";
 import StairTransition from "../../../../components/Portfolio/public/StairTransition";
-// @ts-expect-error: global CSS import side effect; no local type declaration
 import "./scrollbar.css";
 
 interface LayoutProps {
